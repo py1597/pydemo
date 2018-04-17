b2e4ec716c46b6343db0772b43980bfa10b3f1e5
py1597 <1597973097@qq.com>
2018-04-17 14:27:43 +0800

    v1.0

--- demo.py
+++ demo.py
@@ -15,3 +15,8 @@ print("dev--------------------")
 print("hello 忽略部分文件 world4")
 print("hello world4hello world4hello world4")
 print("hello world4hello world4hello world4")
+
+print("v2.0--------------------")
+print("hello 忽略部分文件 world4")
+print("hello world4hello world4hello world4")
+print("hello world4hello world4hello world4")
